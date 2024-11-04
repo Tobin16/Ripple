83cf58c49d0e3ef273c4623ee6b7cbf1a7bffac2
Tobin16 <tobinmathew16@gmail.com>
2024-11-04 13:50:10 +0530

    Add chat container setup

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
+import styled from "styled-components"
 
 const Chat = () => {
   return (
-    <div>
-      chat
-    </div>
+    <>
+      <Container>
+          <div className="container">
+
+          </div>
+      </Container>
+    </>
   )
 }
 
+
+const Container = styled.div`
+height: 100vh;
+width: 100vw;
+display:flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+gap: 1rem;
+background-color: #131324;
+
+.container {
+  background-color: #0d0d19;
+  height: 85vh;
+  width: 85vw;
+}
+
+`;
 export default Chat
